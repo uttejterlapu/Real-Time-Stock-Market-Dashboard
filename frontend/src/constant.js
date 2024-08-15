3ef965890a785c23d8df130fd9cf373be00b99ec
uttejterlapu <uttejterlapu@gmail.com>
2024-08-15 14:59:44 +0530

    added backend url

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,5 @@
 // const BACKENDURL = 'http://localhost:5000'; 
-const BACKENDURL = 'https://stock-market-dashboard-f3us.onrender.com'; 
+const BACKENDURL = 'https://real-time-stock-market-dashboard.onrender.com'; 
 module.exports = {
     BACKENDURL
 }
